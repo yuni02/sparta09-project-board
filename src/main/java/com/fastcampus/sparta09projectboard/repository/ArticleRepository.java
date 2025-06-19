0b149523e7f13837122bbaf899243dbf2c1983fc
Yunkyeong <yunkyeong@dencomm.ai>
2025-06-19 13:07:50 +0900

    #2 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    QUerydslPredicateExecutor, QuerydslBinderCustomizer 를 이용하여 간단하게 개발

--- src/main/java/com/fastcampus/sparta09projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/sparta09projectboard/repository/ArticleRepository.java
@@ -2,7 +2,35 @@ package com.fastcampus.sparta09projectboard.repository;
 
 import com.fastcampus.sparta09projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import com.fastcampus.sparta09projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @RepositoryRestResource// spring data rest 설정 추가
-public interface ArticleRepository extends JpaRepository<Article, Long> {}
+public interface ArticleRepository extends JpaRepository<Article, Long>, QuerydslPredicateExecutor<Article>, QuerydslBinderCustomizer<QArticle> {
+
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+    Page<Article> findByContentContaining(String content, Pageable pageable);
+//    Page<Article> findByUserAccount_UserIdContaining(String userId, Pageable pageable);
+//    Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
+
+//    void deleteByIdAndUserAccount_UserId(Long articleId, String userid);
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.author, root.createdAt, root.createdBy);
+        // likeIgnoreCase -> like '${v}' , containsIgnoreCase -> '%${v}$'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.author).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+}
