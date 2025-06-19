6cd55cc6a19029e90ed913392894f6fd0d7fd4d8
Yunkyeong <yunkyeong@dencomm.ai>
2025-06-19 18:38:20 +0900

    #20 - 게시판 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.

---
+++ src/main/java/com/fastcampus/sparta09projectboard/controller/ArticleController.java
@@ -0,0 +1,11 @@
+package com.fastcampus.sparta09projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/*
+/articles
+/articles/{article-id}
+ */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {}
