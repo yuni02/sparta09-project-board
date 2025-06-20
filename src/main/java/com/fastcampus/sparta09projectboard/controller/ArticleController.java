75ff3d18fd48aaacdca3ed3dce7b98511b540802
Yunkyeong <yunkyeong@dencomm.ai>
2025-06-20 17:57:31 +0900

    #20 - 게시글 뷰 기본 골격 작성
    conroller, view, test 까지 한번에 적용

--- src/main/java/com/fastcampus/sparta09projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/sparta09projectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.fastcampus.sparta09projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,5 +20,11 @@ public class ArticleController {
   public String articles(ModelMap map) {
     map.addAttribute("articles", List.of());
     return "articles/index";
+  }
+
+  @GetMapping("/{articleId}")
+  public String article(@PathVariable Long articleId, ModelMap map) {
+    map.addAttribute("article", "article"); // TODO: 구현시 여기에 실제 데이터 넣어줘야 함.
+    return "articles/detail";
   }
 }
